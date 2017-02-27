f0dda5468641ec46c99d225b712a27bcb6739fe8
PNouvellet <p.nouvellet@imperial.ac.uk>
2017-02-27 12:24:14 +0000

    add extinction probability function

--- R/internals.R
+++ R/internals.R
@@ -2,3 +2,8 @@
 ## These functions are not exported, and are meant for internal use only.
 
 
+# Proba extinction given R and poisson
+proba.ext.poisson <- function(alpha){
+  proba.ext <- exp(-alpha)
+  return(proba.ext)
+}
