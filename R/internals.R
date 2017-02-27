a456a982cb94e52c5c80089e7aa9c2af4b5467f9
Thibaut Jombart <thibautjombart@gmail.com>
2017-02-27 12:32:15 +0000

    using snake case

--- R/internals.R
+++ R/internals.R
@@ -2,8 +2,9 @@
 ## These functions are not exported, and are meant for internal use only.
 
 
-# Proba extinction given R and poisson
-proba.ext.poisson <- function(alpha){
-  proba.ext <- exp(-alpha)
-  return(proba.ext)
+## Probabilty of extinction given R and assuming a Poisson process
+
+proba_ext_poisson <- function(alpha){
+  proba_ext <- exp(-alpha)
+  return(proba_ext)
 }
