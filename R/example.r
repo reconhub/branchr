c4580e908719edfc3f412f37e5b1d62d1325bc57
PNouvellet <p.nouvellet@imperial.ac.uk>
2017-03-31 10:01:36 +0100

    simulation
    first simulation of poisson

---
+++ R/example.r
@@ -0,0 +1,56 @@
+#' Simulate a partially observed Poisson branching process with discrete generations
+#'
+#' For a given reproduction number, this function simulate a Poisson branching and return observed incidence 
+#' (per generation) accounting for under-reporting.
+#' . This is \code{some code}.
+#' Bullet list:
+#'
+#' \itemize{
+#'
+#' \item toto: il est beau
+#'
+#' \item tata: il est caca
+#'
+#' }
+#'
+#'
+#' @author Pierre Nouvellet (\email{p.nouvellet@@imperial.ac.uk})
+#'
+#' @export
+#'
+#' @param x this is what x is
+#'
+#' @param y this is what y is
+#'
+#' @return
+#'  The function returns ....
+#'
+#' @examples
+#'
+#' x <- simulate_poisson(1, 2)
+#' x
+#'
+simulate_poisson <- function(R.eff,n,rho,t.max) {
+  
+  R <- R.eff$R.effective
+  
+  N <- matrix(1,n,t.max)
+  for (i in 2:t.max){
+    N[,i] <- rpois(n,R*N[,i-1])
+  }
+  n.ongoing <- n - sum(N[,t.max]==0)
+  if (n.ongoing>0) warning(
+    paste0('There are ',n.ongoing,' outbreaks that are not extinct, 
+           consider increasing the number of time-steps (especially if R<1).'))
+  
+  z.sim <- rowSums(N)
+  y.obs.full <- rbinom(n,z.sim,rho)
+  f <- which(y.obs.full==0)
+  if (length(f)>0){
+    y.obs <- y.obs.full[-f]
+  }else{
+    y.obs <- y.obs.full
+  }
+  return( list(outbreak.size = z.sim, observed.size.full = y.obs.full,
+               observed.size = y.obs) ) 
+}
--- R/simulate_poisson.R
+++ R/simulate_poisson.R
@@ -1,33 +1,85 @@
-#' Simulate bla bla (title)
+#' Simulate a partially observed Poisson branching process with discrete generations
 #'
-#' This is what it does bla bla (descripion). This is \code{some code}.
-#' Bullet list:
-#'
-#' \itemize{
-#'
-#' \item toto: il est beau
-#'
-#' \item tata: il est caca
-#'
-#' }
+#' For a given reproduction number, this function simulate the incidence of a disease at each generation. 
+#' The offspring distribution of each individual infected is assumed to follow a Poisson distribution with mean
+#' equal to the reproduction number. To simulate under-reporting, users can specify a probability to observe case,
+#'  this reporting probability is assumed constant through the simulation.
 #'
 #'
 #' @author Pierre Nouvellet (\email{p.nouvellet@@imperial.ac.uk})
 #'
 #' @export
 #'
-#' @param x this is what x is
+#' @param R is the reproduction number, i.e. the average number of secondary cases due to a single case. 
+#' This can be any positive number. if R is a vector, then the length of R must be 'n' (see below). 
 #'
-#' @param y this is what y is
+#' @param n is the number of simulations performed. Each simulation start with 's'  individual(s) infected. 
+#' 'n' must be a positive integer. 
+#'
+#' @param s is the number individual(s) initially infected. if s is a vector, then the length of s must be 'n' (see above).
+#'
+#' @param rho is the probability (between 0 and 1) that an incident case is observed, i.e. reporting probability.
+#' if rho is a vector, then the length of rho must be 'n' (see below)
+#'
+#' @param t_max is the maximum number of generations simulated. 't_max' must be a positive integer. 
 #'
 #' @return
-#'  The function returns ....
+#'  The function returns a list including:
+#' \itemize{
+#'
+#' \item z_sim: the final outbreak sizes for each simulated outbreaks. Note that for outbreaks that are not extinct
+#' 'final' by the end of the simulation, z_sim correspond to the cumulative incidence by time t_max.
+#'
+#' \item tata: il est caca
+#'
+#' }#'observed incidence (per generation) accounting for under-reporting.
+#' If some simulated outbreak are not extinct by the end of the simulation, a warning is displayed.
 #'
 #' @examples
 #'
-#' x <- simulate_poisson(1, 2)
+#' x <- simulate_poisson(.8, 2)
 #' x
 #'
-simulate_poisson <- function(x, y) {
-  return(x + y)
+simulate_poisson <- function(R,n,s,rho,t_max) {
+	# check input
+	if (length(R)!=1 | length(R)!=n) warning('R must be a vector, and its size must be either 1 or n')
+	if (length(s)!=1 | length(s)!=n) warning('s must be a vector, and its size must be either 1 or n')
+	if (length(rho)!=1 | length(rho)!=n) warning('rho must be a vector, and its size must be either 1 or n')
+	if (length(n)!=1 | n=0 | !is.integer(n)) warning('n must be a positive integer')
+	if (length(t_max)!=1 | t_max=0 | !is.integer(T_max)) warning('t_max must be a positive integer')
+
+	# declare incidence matrices
+  N <- matrix(NA,n,t.max)
+  N_obs_full <- matrix(NA,n,t.max)
+
+	# initial values
+  N[,1] <- s
+  N_obs_full[,1] <- rbinom(n,N[,1],rho)
+  
+	# simulate the Poisson branching process and reporting
+  for (i in 2:t.max){
+    N[,i] <- rpois(n,R*N[,i-1])
+	N_obs_full[,i] <- rbinom(n,N[,i],rho)
+  }
+	# check all simulated outbreaks are extinct
+  n.ongoing <- n - sum(N[,t.max]==0)
+  if (n.ongoing>0) warning(
+    paste0('There are ',n.ongoing,' outbreaks that are not extinct, 
+           consider increasing the number of time-steps (especially if R<1).'))
+	
+	# calculate final sizes, true and observed
+  z <- rowSums(N)
+  z_obs_full <- rowSums(N_obs_full)
+  
+	# correct for unobserved outbreaks
+  f <- which(y.obs.full==0)
+  if (length(f)>0){
+    y.obs <- y.obs.full[-f]
+  }else{
+    y.obs <- y.obs.full
+  }
+
+	# output
+  return( list(true_incidence = N, reported_incidence = N_obs_full, observed_incidence = N_obs, 
+               true_size = z, reported_size = z_obs_full, observed_size = z_obs) ) 
 }
